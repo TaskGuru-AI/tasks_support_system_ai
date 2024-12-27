808b1db1bc411f61eea03af6a042e8498337b84d
Feodosiya <feomak16@gmail.com>
2024-12-27 16:26:20 +0300

    Tab overview NLP

--- tasks_support_system_ai/web/pages/nlp.py
+++ tasks_support_system_ai/web/pages/nlp.py
@@ -1,12 +1,12 @@
 import numpy as np
-from tasks_support_system_ai.web.tabs import nlp_overview
 import streamlit as st
+
+from tasks_support_system_ai.web.tabs import nlp_overview
 
 tab1, tab2, tab3 = st.tabs(["Project info", "🗃️ Train", "📊 Prediction"])
 data = np.random.randn(10, 1)
 
-with tab1:
-    nlp_overview()
+nlp_overview.nlp_overview()
 
 tab2.subheader("Описание датасета")
 tab2.write(data)
