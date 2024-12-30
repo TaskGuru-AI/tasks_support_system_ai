7d5cf57cd274871e47229e1a0a0a861f92b019ce
Ilia Ilin <46445792+ilyinily@users.noreply.github.com>
2024-12-30 13:26:38 +0300

    Update nlp.py
    to deal with linter requirements part 7

--- tasks_support_system_ai/web/pages/nlp.py
+++ tasks_support_system_ai/web/pages/nlp.py
@@ -7,7 +7,6 @@ import streamlit as st
 from tasks_support_system_ai.web.tabs.nlp_overview import show_overview
 from tasks_support_system_ai.web.tabs.nlp_train import render_train_tab
 
-
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -20,10 +19,9 @@ with tab1:
 with tab2:
     render_train_tab()
 
-#TODO: reformat this so it looks like in Feodosiya's code
+# TODO: reformat this so it looks like in Feodosiya's code
 
 # TAB 3 SECTION - PREDICT
-
 
 
 # NEEDED FUNCTIONS
@@ -35,6 +33,7 @@ with tab2:
 #         nlp_predict_input = nlp_predict_form.file_uploader(label="CSV:",
 #                                                            type=["csv"])
 
+
 def nlp_display_collected_params(*args):
     tab3.write("These are the parameters stored in Predict function now:")
     tab3.write(args)
@@ -45,38 +44,44 @@ def nlp_display_collected_params(*args):
 #                              enter_to_submit=True,
 #                              border=True)
 
-nlp_predict_model_selector = tab3.selectbox(label="Choose model:",
-                                            placeholder="Choose model",
-                                            options=["model_id_1", "model_id_2"],
-                                            index=None) # This should be the list of models
+nlp_predict_model_selector = tab3.selectbox(
+    label="Choose model:",
+    placeholder="Choose model",
+    options=["model_id_1", "model_id_2"],
+    index=None,
+)  # This should be the list of models
 
-nlp_input_selector = tab3.radio(label="Text or CSV file:",
-                                            options=["Text", "CSV"],
-                                            index=0,
-                                            horizontal=True)
+nlp_input_selector = tab3.radio(
+    label="Text or CSV file:", options=["Text", "CSV"], index=0, horizontal=True
+)
 
 # Kept both text input and file selector as user may decide mid-action they need them
 
-nlp_predict_input_text = tab3.text_area(label="Text:",
-                                        help="Please provide text of the conversation")
+nlp_predict_input_text = tab3.text_area(
+    label="Text:", help="Please provide text of the conversation"
+)
 
-nlp_predict_input_file_uploader = tab3.file_uploader(label="CSV:",
-                                                     type=["csv", "py"],
-                                                     help="Please provide a CSV file")
+nlp_predict_input_file_uploader = tab3.file_uploader(
+    label="CSV:", type=["csv", "py"], help="Please provide a CSV file"
+)
 
-nlp_predict_button = tab3.button(label=f"Predict from {nlp_input_selector}",
-                                 type="primary",
-                                 on_click=nlp_display_collected_params, # This calls API
-args=(nlp_predict_model_selector, nlp_input_selector, nlp_predict_input_text))
+nlp_predict_button = tab3.button(
+    label=f"Predict from {nlp_input_selector}",
+    type="primary",
+    on_click=nlp_display_collected_params,  # This calls API
+    args=(nlp_predict_model_selector, nlp_input_selector, nlp_predict_input_text),
+)
 
-nlp_clear_form_button = tab3.button(label="Clear form",
-                                    type="secondary")
+nlp_clear_form_button = tab3.button(label="Clear form", type="secondary")
 
-nlp_prediction_results = tab3.table(pd.DataFrame({"Text": ["Please buy this cream",
-                                                           "I want a new SIM card",
-                                                           "It doesn't work"],
-                                                  "Cluster": ["Spam", "Request", "Tech work"]}))
-
+nlp_prediction_results = tab3.table(
+    pd.DataFrame(
+        {
+            "Text": ["Please buy this cream", "I want a new SIM card", "It doesn't work"],
+            "Cluster": ["Spam", "Request", "Tech work"],
+        }
+    )
+)
 
 
 # tab3.subheader("Модель классификации")
