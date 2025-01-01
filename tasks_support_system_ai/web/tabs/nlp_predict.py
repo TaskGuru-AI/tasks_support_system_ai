fe72c4435cfca5ecd21ac2dc8bfa60cdb2acfb37
Ilia Ilin <46445792+ilyinily@users.noreply.github.com>
2025-01-02 02:39:16 +0300

    Update nlp_predict.py
    deal with linter take 5

--- tasks_support_system_ai/web/tabs/nlp_predict.py
+++ tasks_support_system_ai/web/tabs/nlp_predict.py
@@ -20,29 +20,24 @@ def predict_pick_model():
             label="Выберите модель для предсказания:",
             options=st.session_state.model_configs.values(),
             index=None,
-            key="nlp_pick_model"
+            key="nlp_pick_model",
         )
 
 
 def nlp_input_selector():
     if st.session_state.model_ids:
         st.session_state.nlp_selected_input = st.radio(
-            label="Выберите, с чем работать:",
-            options=["Текст", "CSV"],
-            index=0,
-            horizontal=True
+            label="Выберите, с чем работать:", options=["Текст", "CSV"], index=0, horizontal=True
         )
+
 
 def nlp_show_input_options():
     if st.session_state.model_ids:
         st.session_state.nlp_predict_input_text = st.text_area(
-            label="Текст:",
-            help="Впишите текст обращения"
+            label="Текст:", help="Впишите текст обращения"
         )
         st.session_state.nlp_predict_input_file_uploader = st.file_uploader(
-            label="CSV:",
-            type=["csv", "py"],
-            help="Добавьте CSV-файл"
+            label="CSV:", type=["csv", "py"], help="Добавьте CSV-файл"
         )
 
 
@@ -52,12 +47,10 @@ def nlp_predict_clear_buttons():
             label=f"Предсказать с источника: {st.session_state.nlp_selected_input}",
             type="primary",
             on_click=nlp_display_collected_params,
-            args=(st.session_state.picked_model, st.session_state.nlp_selected_input)
+            args=(st.session_state.picked_model, st.session_state.nlp_selected_input),
         )
-        st.button(
-            label="Сбросить ввод",
-            type="secondary"
-        )
+        st.button(label="Сбросить ввод", type="secondary")
+
 
 nlp_sample_results = st.table(
     pd.DataFrame(
@@ -79,11 +72,8 @@ def render_predict_tab():
     if "nlp_predict_input_file_uploader" not in st.session_state:
         st.session_state.nlp_predict_input_file_uploader = ""
     if not st.session_state.model_ids:
-        st.subheader(
-            body="Для выполнения предсказания обучите хотя бы одну модель"
-        )
+        st.subheader(body="Для выполнения предсказания обучите хотя бы одну модель")
     predict_pick_model()
     nlp_input_selector()
     nlp_show_input_options()
     nlp_predict_clear_buttons()
-
