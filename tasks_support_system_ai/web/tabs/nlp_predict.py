9e71be9fe9b80ab39e4d666e44da6639fd047148
Ilia Ilin <46445792+ilyinily@users.noreply.github.com>
2025-01-19 03:31:27 +0300

    Update nlp_predict.py with multiple features
    reworked predict buttons display
    multi-cluster result display now possible for text prediction
    csv prediction now yields a table
    csv prediction results can be now downloaded
    upon model selection, its parameters are displayed

--- tasks_support_system_ai/web/tabs/nlp_predict.py
+++ tasks_support_system_ai/web/tabs/nlp_predict.py
@@ -1,12 +1,27 @@
+import io
 import os
 
 import pandas as pd
+import requests
 import streamlit as st
 
 BACKEND_URL = "http://backend:8000/nlp"
 
 if os.getenv("IS_DOCKER", "0") == "0":
     BACKEND_URL = "http://localhost:8000/nlp"
+
+CLUSTERS_MAP = {
+    1: "Списания/денежные средства",
+    2: "Проблемы с сим-меню",
+    3: "Esim (проблемы подключения, код активации, qr-код)",
+    4: "Проблемы с контентом и отключение услуг/ подписок/ сервисов",
+    5: "Учетные записи",
+    6: "Уведомления о технических работах",
+    7: "Сбой работы (в работе оператора)",
+    8: "Тесты, проблемы с доставкой, прочие тех. оповещения",
+    9: "Черный список",
+    10: "Спам, реклама",
+}
 
 
 def nlp_display_collected_params(*args):
@@ -18,16 +33,14 @@ def predict_pick_model():
     if st.session_state.model_ids:
         st.session_state.picked_model = st.selectbox(
             label="Выберите модель для предсказания:",
-            options=st.session_state.model_configs.values(),
+            options=st.session_state.model_ids,
             index=None,
             key="nlp_pick_model",
         )
-
-
-def nlp_input_selector():
-    if st.session_state.model_ids:
-        st.session_state.nlp_selected_input = st.radio(
-            label="Выберите, с чем работать:", options=["Текст", "CSV"], index=0, horizontal=True
+    if st.session_state.picked_model:
+        st.success(
+            f"Выбранные параметры предсказания:\n"
+            f"{st.session_state.model_configs[st.session_state.picked_model]}"
         )
 
 
@@ -37,29 +50,82 @@ def nlp_show_input_options():
             label="Текст:", help="Впишите текст обращения"
         )
         st.session_state.nlp_predict_input_file_uploader = st.file_uploader(
-            label="CSV:", type=["csv", "py"], help="Добавьте CSV-файл"
+            label="CSV:", type=["csv"], help="Добавьте CSV-файл"
         )
 
 
-def nlp_predict_clear_buttons():
+def nlp_predict_from_text(model_id, input_text):
+    with st.spinner("Предсказываем..."):
+        try:
+            response = requests.post(
+                url=f"{BACKEND_URL}/api/predict_nlp", json={"id": model_id, "text": input_text}
+            )
+            st.session_state["cluster_from_text"] = response.json()["clusters"]
+
+        except Exception as e:
+            st.error(f"Ошибка при формировании предсказания из текста: {e}")
+
+
+def nlp_predict_from_csv(model_id, input_file):
+    with st.spinner("Предсказываем..."):
+        try:
+            response = requests.post(
+                url=f"{BACKEND_URL}/api/predict_nlp_csv?id={model_id}", files={"file": input_file}
+            )
+            st.session_state["cluster_from_csv"] = response.content.decode("utf-8")
+            st.session_state["formatted_cluster_from_csv"] = pd.read_csv(
+                io.StringIO(st.session_state["cluster_from_csv"]), sep=","
+            )
+            st.session_state["formatted_cluster_from_csv"]["prediction_desc"] = st.session_state[
+                "formatted_cluster_from_csv"
+            ]["prediction"].map(CLUSTERS_MAP)
+
+        except Exception as e:
+            st.error(f"Ошибка при формировании предсказания из файла: {e}")
+
+
+def nlp_predict_buttons():
     if st.session_state.model_ids:
-        st.button(
-            label=f"Предсказать с источника: {st.session_state.nlp_selected_input}",
-            type="primary",
-            on_click=nlp_display_collected_params,
-            args=(st.session_state.picked_model, st.session_state.nlp_selected_input),
-        )
-        st.button(label="Сбросить ввод", type="secondary")
+        nlp_predict_button_column1, nlp_predict_button_column2 = st.columns(2)
+        with nlp_predict_button_column1:
+            if st.button(label="Предсказать на основе текста", type="primary"):
+                nlp_predict_from_text(
+                    st.session_state.picked_model, st.session_state.nlp_predict_input_text
+                )
+        with nlp_predict_button_column2:
+            if st.button(label="Предсказать на основе CSV-файла", type="primary"):
+                nlp_predict_from_csv(
+                    st.session_state.picked_model, st.session_state.nlp_predict_input_file_uploader
+                )
 
 
-nlp_sample_results = st.table(
-    pd.DataFrame(
-        {
-            "Text": ["Please buy this cream", "I want a new SIM card", "It doesn't work"],
-            "Cluster": ["Spam", "Request", "Tech work"],
-        }
-    )
-)
+def nlp_display_result_text():
+    if "cluster_from_text" in st.session_state:
+        if len(st.session_state["cluster_from_text"]) == 1:
+            with st.expander(label="Для введённого текста был определён следующий кластер:"):
+                st.write(f"{CLUSTERS_MAP[st.session_state["cluster_from_text"][0]]}")
+        else:
+            with st.expander(label="Для введённого текста были определены следующие кластеры:"):
+                for class_from_text in st.session_state["cluster_from_text"]:
+                    st.write(f"{CLUSTERS_MAP[class_from_text]}")
+
+
+def nlp_display_result_csv():
+    if "cluster_from_csv" in st.session_state:
+        with st.expander(label="Для загруженного файла классы были определены так:"):
+            st.dataframe(
+                st.session_state["formatted_cluster_from_csv"],
+                hide_index=True,
+                use_container_width=True,
+            )
+            st.download_button(
+                label="Скачать",
+                data=st.session_state["formatted_cluster_from_csv"]
+                .to_csv(index=False)
+                .encode("utf-8"),
+                file_name="nlp_predicted_from_csv.csv",
+                mime="text/csv",
+            )
 
 
 def render_predict_tab():
@@ -74,6 +140,8 @@ def render_predict_tab():
     if not st.session_state.model_ids:
         st.subheader(body="Для выполнения предсказания обучите хотя бы одну модель")
     predict_pick_model()
-    nlp_input_selector()
+    # nlp_input_selector()
     nlp_show_input_options()
-    nlp_predict_clear_buttons()
+    nlp_predict_buttons()
+    nlp_display_result_text()
+    nlp_display_result_csv()
